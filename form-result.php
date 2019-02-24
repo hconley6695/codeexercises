178740a8ea888434ab4ed933053668a48bb2c917
Heather Conley <hlconley@Heathers-MacBook-Air.local>
2019-02-24 16:40:34 -0500

    ensured that processing of form would go to the form-result page

---
+++ form-result.php
@@ -0,0 +1,10 @@
+<?php 
+	
+	$name = $_POST["name"];
+	$phone = $_POST["phone"];
+	$email = $_POST["email"];
+
+	echo "My name is " . $name . "<br/>";
+	echo "My email address is " . $email . "<br/>";
+	echo "My phone number is " . $phone . "<br/>";
+ ?>
