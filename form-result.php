04cd10ca4020b58075133288c07216762f8dc2ed
Heather Conley <hlconley@Heathers-MacBook-Air.local>
2019-02-28 19:13:37 -0500

    added more form fields and made sure that php was receiving them correctly

--- form-result.php
+++ form-result.php
@@ -3,8 +3,15 @@
 	$name = $_POST["name"];
 	$phone = $_POST["phone"];
 	$email = $_POST["email"];
+	$animal = $_POST["animal"];
+	$color = $_POST["color"];
+
 
 	echo "My name is " . $name . "<br/>";
 	echo "My email address is " . $email . "<br/>";
 	echo "My phone number is " . $phone . "<br/>";
+	echo "My favorite animal is a " . $animal . "<br/>";
+	// echo "<pre>";
+	// print_r($animal);
+	// echo "</pre>";
  ?>
--- index.php
+++ index.php
@@ -5,16 +5,19 @@
 	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+
 </head>
 <body>
 	<div class="body-wrapper">
 		<button class="btn">Click Here for a Pop-up</button>
 		<div class="popup-wrapper">
 			<div class="title">Help Us Get to Know You!</div>
-			<form name="sample-form" class="sample-form"> 
+			<form name="sample-form" class="sample-form" action="form-result.php" method="post"> 
 				<div>
 					<span class="error">This field is required.</span>
-					<input type="text" name="full-name" placeholder="Full Name">
+					<input type="text" name="name" placeholder="Full Name">
 				</div>
 				<div>
 					<span class="error">This field is required.</span>
@@ -25,16 +28,58 @@
 					<input type="tel" name="phone" placeholder="Phone">
 				</div>
 				<div class="select-group">
-					<span>This field is required.</span>
-					<select>
+					<span class="error">This field is required.</span>
+					<select name="animal">
 						<option value="animal" default>Choose your favorite animal.</option>
 						<option value="dog">Dog</option>
 						<option value="cat">Cat</option>
 						<option value="horse">Horse</option>
 						<option value="turtle">Turtle</option>
-						<option vaue="fish">Fish</option>
+						<option value="fish">Fish</option>
 					</select>
 				</div>
+
+
+				<div>
+					<p>Which color do you prefer?</p>
+					<span class="error">This field is required.</span>
+					<label for="red">red</label>
+					<input type="radio" name="color" value="red" id="red">
+					<label for="yellow">yellow</label>
+					<input type="radio" name="color" value="yellow" id="yellow">
+					<label for="blue">blue</label>
+					<input type="radio" name="color" value="blue" id="blue">
+					<label for="green">green</label>
+					<input type="radio" name="color" value="green" id="green">
+
+				</div>
+
+				<div>
+					<p>When is your birthday?</p>
+					<span class="error">This field is required.</span>
+					<input type="date" name="birthday" id="birthday">
+				</div>
+
+				<div>
+					<p>What music genres do you like?  Check all that apply.</p>
+					<span class="error">Please check at least one type of music.</span>
+					<input type="checkbox" name="classical" id="classical">
+					<label for="classical">Classical</label>
+
+					<input type="checkbox" name="classicrock" id="classicrock">
+					<label for="classicrock">Classic Rock</label>
+
+					<input type="checkbox" name="alternative" id="alternative">
+					<label for="alternative">Alternative Rock</label>
+
+					<input type="checkbox" name="country" id="country">
+					<label for="country">Country</label>
+
+					<input type="checkbox" name="hiphop" id="hiphop">
+					<label for="hiphop">Hip Hop</label>
+
+				</div>
+
 				<button class="popup-btn">Submit</button>
 			</form>
 		</div>
@@ -45,6 +90,12 @@
 	jQuery(document).ready(function() {
 
 		// jQuery("div.popup-wrapper").css("display", "none");
+
+		jQuery("#birthday").datepicker({
+			changeMonth: true,
+		    changeYear: true,
+		    yearRange: "1920:2019"
+		});
 
 
 	});
