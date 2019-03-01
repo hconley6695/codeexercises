272cc406dac2b40c7c4f562d830e1c5bf0591dce
Heather Conley <hlconley@Heathers-MacBook-Air.local>
2019-02-28 19:57:21 -0500

    added pop-up functionality to the form, styled the pop-up so the form was centered on the screen

--- index.php
+++ index.php
@@ -12,77 +12,83 @@
 <body>
 	<div class="body-wrapper">
 		<button class="btn">Click Here for a Pop-up</button>
-		<div class="popup-wrapper">
-			<div class="title">Help Us Get to Know You!</div>
-			<form name="sample-form" class="sample-form" action="form-result.php" method="post"> 
-				<div>
-					<span class="error">This field is required.</span>
-					<input type="text" name="name" placeholder="Full Name">
-				</div>
-				<div>
-					<span class="error">This field is required.</span>
-					<input type="email" name="email" placeholder="Email">
-				</div>
-				<div>
-					<span class="error">This field is required.</span>
-					<input type="tel" name="phone" placeholder="Phone">
-				</div>
-				<div class="select-group">
-					<span class="error">This field is required.</span>
-					<select name="animal">
-						<option value="animal" default>Choose your favorite animal.</option>
-						<option value="dog">Dog</option>
-						<option value="cat">Cat</option>
-						<option value="horse">Horse</option>
-						<option value="turtle">Turtle</option>
-						<option value="fish">Fish</option>
-					</select>
-				</div>
+		<div id="popup-wrapper-bkgrd">
+			
+			<div class="popup-wrapper">
+				<div id="exit">x</div>
+				<div class="title">Help Us Get to Know You!</div>
+				<form name="sample-form" class="sample-form" action="form-result.php" method="post"> 
+					<div>
+						<span class="error">This field is required.</span>
+						<input type="text" name="name" placeholder="Full Name">
+					</div>
+					<div>
+						<span class="error">This field is required.</span>
+						<input type="email" name="email" placeholder="Email">
+					</div>
+					<div>
+						<span class="error">This field is required.</span>
+						<input type="tel" name="phone" placeholder="Phone">
+					</div>
+					<div class="select-group">
+						<span class="error">This field is required.</span>
+						<select name="animal">
+							<option value="animal" default>Choose your favorite animal.</option>
+							<option value="dog">Dog</option>
+							<option value="cat">Cat</option>
+							<option value="horse">Horse</option>
+							<option value="turtle">Turtle</option>
+							<option value="fish">Fish</option>
+						</select>
+					</div>
 
 
-				<div>
-					<p>Which color do you prefer?</p>
-					<span class="error">This field is required.</span>
-					<label for="red">red</label>
-					<input type="radio" name="color" value="red" id="red">
-					<label for="yellow">yellow</label>
-					<input type="radio" name="color" value="yellow" id="yellow">
-					<label for="blue">blue</label>
-					<input type="radio" name="color" value="blue" id="blue">
-					<label for="green">green</label>
-					<input type="radio" name="color" value="green" id="green">
+					<div>
+						<p>Which color do you prefer?</p>
+						<span class="error">This field is required.</span>
+						<label for="red">red</label>
+						<input type="radio" name="color" value="red" id="red">
+						<label for="yellow">yellow</label>
+						<input type="radio" name="color" value="yellow" id="yellow">
+						<label for="blue">blue</label>
+						<input type="radio" name="color" value="blue" id="blue">
+						<label for="green">green</label>
+						<input type="radio" name="color" value="green" id="green">
 
-				</div>
+					</div>
 
-				<div>
-					<p>When is your birthday?</p>
-					<span class="error">This field is required.</span>
-					<input type="date" name="birthday" id="birthday">
-				</div>
+					<div>
+						<p>When is your birthday?</p>
+						<span class="error">This field is required.</span>
+						<input type="date" name="birthday" id="birthday">
+					</div>
 
-				<div>
-					<p>What music genres do you like?  Check all that apply.</p>
-					<span class="error">Please check at least one type of music.</span>
-					<input type="checkbox" name="classical" id="classical">
-					<label for="classical">Classical</label>
+					<div>
+						<p>What music genres do you like?  Check all that apply.</p>
+						<span class="error">Please check at least one type of music.</span>
+						<input type="checkbox" name="classical" id="classical">
+						<label for="classical">Classical</label>
 
-					<input type="checkbox" name="classicrock" id="classicrock">
-					<label for="classicrock">Classic Rock</label>
+						<input type="checkbox" name="classicrock" id="classicrock">
+						<label for="classicrock">Classic Rock</label>
 
-					<input type="checkbox" name="alternative" id="alternative">
-					<label for="alternative">Alternative Rock</label>
+						<input type="checkbox" name="alternative" id="alternative">
+						<label for="alternative">Alternative Rock</label>
 
-					<input type="checkbox" name="country" id="country">
-					<label for="country">Country</label>
+						<input type="checkbox" name="country" id="country">
+						<label for="country">Country</label>
 
-					<input type="checkbox" name="hiphop" id="hiphop">
-					<label for="hiphop">Hip Hop</label>
+						<input type="checkbox" name="hiphop" id="hiphop">
+						<label for="hiphop">Hip Hop</label>
 
-				</div>
+					</div>
 
-				<button class="popup-btn">Submit</button>
-			</form>
+					<button class="popup-btn">Submit</button>
+				</form>
+			</div>
+
 		</div>
+
 	</div>
 
 <script type="text/javascript">
@@ -97,6 +103,15 @@
 		    yearRange: "1920:2019"
 		});
 
+		jQuery("div.body-wrapper .btn").on("click", function() {
+			jQuery("#popup-wrapper-bkgrd").css("display", "block");
+			// jQuery("#popup-wrapper-bkgrd").fadeIn("slow");
+		});
+
+		jQuery("#exit").on("click", function() {
+			jQuery("#popup-wrapper-bkgrd").css("display", "none");
+			// jQuery("#popup-wrapper-bkgrd").fadeOut("slow");
+		});
 
 	});
 </script>
